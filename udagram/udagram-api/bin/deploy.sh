140bd307c33f3278ea5baa24cc20497a7b0b4eb8
salsabeel95 <huss95salsabeel@gmail.com>
2023-02-16 03:16:23 +0200

    trying to fix setting env. variables command

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,7 +1,7 @@
 cd www
 eb init udagram-api --region us-east-1 --platform "node.js-14"
-cd ..
-# this command will set every variable set already on the .env file
-eb setenv `cat .env | sed '/^#/ d' | sed '/^$/ d'`
-cd www
+
+# this command will set env variables
+eb setenv POSTGRES_HOST=database-1.crrpbvktsrdq.us-east-1.rds.amazonaws.com POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=tempDB AWS_DEFAULT_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=elasticbeanstalk-us-east-1-665058644940 URL=http://udgram-sals.s3-website-us-east-1.amazonaws.com/ JWT_SECRET=ToP-SeCeRt AWS_ACCESS_KEY_ID=AKIAZVWEVSPGOJKZQ2O5 AWS_SECRET_ACCESS_KEY=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 EB_APP_NAME=udagram-api EB_ENV_NAME=udagram-api-dev 
+
 eb deploy udagram-api-dev
