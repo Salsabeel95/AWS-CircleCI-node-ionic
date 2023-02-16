694ef93b1961090a1e4f9674c093106a6e55779f
salsabeel95 <huss95salsabeel@gmail.com>
2023-02-16 03:07:50 +0200

    trying to fix setting env. variables command

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,7 @@
 cd www
 eb init udagram-api --region us-east-1 --platform "node.js-14"
+cd ..
 # this command will set every variable set already on the .env file
-eb setenv `cat ../.env | sed '/^#/ d' | sed '/^$/ d'`
+eb setenv `cat .env | sed '/^#/ d' | sed '/^$/ d'`
+cd www
 eb deploy udagram-api-dev
