517eefa3e48008579775cd93f1d493c3f1ef63db
salsabeel95 <huss95salsabeel@gmail.com>
2023-02-16 02:55:25 +0200

    try to fix lint

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,3 +1,5 @@
 cd www
 eb init udagram-api --region us-east-1 --platform "node.js-14"
+# this command will set every variable set already on the .env file
+eb setenv `cat .env | sed '/^#/ d' | sed '/^$/ d'`
 eb deploy udagram-api-dev
