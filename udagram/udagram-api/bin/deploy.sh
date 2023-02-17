8ddfcf0f8cf784480081db2f04f70a9b0af475cf
salsabeel95 <huss95salsabeel@gmail.com>
2023-02-17 20:48:45 +0200

    updating aws resources urls

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -2,6 +2,8 @@ cd www
 eb init udagram-api --region us-east-1 --platform "node.js-14"
 
 # this command will set env variables
-eb setenv POSTGRES_HOST=database-1.crrpbvktsrdq.us-east-1.rds.amazonaws.com POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=tempDB AWS_DEFAULT_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=elasticbeanstalk-us-east-1-665058644940 URL=http://udgram-sals.s3-website-us-east-1.amazonaws.com/ JWT_SECRET=ToP-SeCeRt AWS_ACCESS_KEY_ID=AKIAZVWEVSPGOJKZQ2O5 AWS_SECRET_ACCESS_KEY=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 EB_APP_NAME=udagram-api EB_ENV_NAME=udagram-api-dev 
+eb setenv POSTGRES_HOST=database-1.crrpbvktsrdq.us-east-1.rds.amazonaws.com POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udgDB AWS_DEFAULT_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=elasticbeanstalk-us-east-1-665058644940 URL=http://udgram-sals.s3-website-us-east-1.amazonaws.com/ JWT_SECRET=ToP-SeCeRt AWS_ACCESS_KEY_ID=AKIAZVWEVSPGCGBYBAKE AWS_SECRET_ACCESS_KEY=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 EB_APP_NAME=udagram-api EB_ENV_NAME=udagram-api-dev 
 
 eb deploy udagram-api-dev
+
+#cd ..
--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,3 @@
-aws s3 cp --recursive ./www s3://udgram-sals
+aws s3 cp --recursive ./www s3://udagram-sals/
 # The trailing / at the end of the URL is important. 
 aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udgram-sals
