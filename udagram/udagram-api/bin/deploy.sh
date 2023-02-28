f06093d7b79151506a3b1b32fde608d268b95b15
salsabeel95 <huss95salsabeel@gmail.com>
2023-02-28 19:01:14 +0200

    update circleci to deploy backend only

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,9 +1,14 @@
 cd www
-eb init udagram-api --region us-east-1 --platform "node.js-14"
+        # eb init udagram-api --region us-east-1 --platform "node.js-14"
+        # eb use udagram-api-env
+# set env variables
+eb setenv POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udgDB
+eb setenv POSTGRES_HOST=database-1.crrpbvktsrdq.us-east-1.rds.amazonaws.com
+eb setenv AWS_ACCESS_KEY_ID=AKIAZVWEVSPGDAXBGIRK AWS_SECRET_ACCESS_KEY=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
+eb setenv AWS_BUCKET=elasticbeanstalk-us-east-1-665058644940
+eb setenv AWS_DEFAULT_REGION=us-east-1 AWS_PROFILE=default
+eb setenv URL=http://udagram-sals.s3-website-us-east-1.amazonaws.com/ JWT_SECRET=ToP-SeCeRt
 
-# this command will set env variables
-eb setenv POSTGRES_HOST=database-1.crrpbvktsrdq.us-east-1.rds.amazonaws.com POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres POSTGRES_DB=udgDB AWS_DEFAULT_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=elasticbeanstalk-us-east-1-665058644940 URL=http://udgram-sals.s3-website-us-east-1.amazonaws.com/ JWT_SECRET=ToP-SeCeRt AWS_ACCESS_KEY_ID=AKIAZVWEVSPGCGBYBAKE AWS_SECRET_ACCESS_KEY=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 EB_APP_NAME=udagram-api EB_ENV_NAME=udagram-api-dev 
+eb deploy udagram-api-env
 
-eb deploy udagram-api-dev
-
-#cd ..
+# cd ..
