51a07744a185a45b28b8d9a33eadd29825593672
salsabeel95 <huss95salsabeel@gmail.com>
2023-03-03 07:32:05 +0200

    updating apiHost

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost:'http://udagram-env.eba-w2dbexee.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost:'Udagramapi-env.eba-btasqbkg.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
