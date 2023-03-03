f6d06fc054a978e931fdcfc235b1c5b1b063c085
salsabeel95 <huss95salsabeel@gmail.com>
2023-03-03 07:01:17 +0200

    try if statment in deploy.sh

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,8 +1,21 @@
 cd www
 
 eb list
-eb use $EB_ENV_NAME
-
+# eb use $EB_ENV_NAME
+if [[ ! -v XXX ]]; then
+    echo "XXX is not set"
+elif [[ -z "$XXX" ]]; then
+    echo "XXX is set to the empty string"
+else
+    echo "XXX has the value: $XXX"
+fi
+if [[ ! -v POSTGRES_DB ]]; then
+    echo "POSTGRES_DB is not set"
+elif [[ -z "$POSTGRES_DB" ]]; then
+    echo "POSTGRES_DB is set to the empty string"
+else
+    echo "POSTGRES_DB has the value: $POSTGRES_DB"
+fi
 # setting env variables in eb env
 # eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB
 # eb setenv POSTGRES_HOST=$POSTGRES_HOST
@@ -12,6 +25,6 @@ eb use $EB_ENV_NAME
 # eb setenv JWT_SECRET=$JWT_SECRET
 
 
-eb deploy 
+# eb deploy 
 
 cd ..
